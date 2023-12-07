a5a16820ce9aeaa4912e7ba898d4d2b7a3298879
Rashhmithaaa <rashmitharj05@gmail.com>
2023-12-07 11:39:23 +0530

    cloning of app using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="https://th.bing.com/th/id/OIP.D1wT2GtuWDP-v16wWu_HvQHaHa?w=180&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'techbite';
+  title = 'homes';
 }
+
