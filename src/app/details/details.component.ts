1288fa5e3dc128b008a6e359f67507100c29d3b1
Rashhmithaaa <rashmitharj05@gmail.com>
2023-12-07 17:11:24 +0530

    CSS changes

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -17,11 +17,11 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
       <img class="listing-photo" [src]="housingLocation?.photo"
         alt="Exterior photo of {{housingLocation?.name}}"/>
       <section class="listing-description">
-        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <h4 class="listing-heading">{{housingLocation?.name}}</h4>
         <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
       </section>
       <section class="listing-features">
-        <h2 class="section-heading">About this housing location</h2>
+        <h2 class="section-heading">About this housing location:</h2>
         <ul>
           <li>Units available: {{housingLocation?.availableUnits}}</li>
           <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
@@ -29,7 +29,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
         </ul>
       </section>
       <section class="listing-apply">
-        <h2 class="section-heading">Apply now to live here</h2>
+        <h2 class="section-heading">Apply now to live here!!</h2>
         <form [formGroup]="applyForm" (submit)="submitApplication()">
           <label for="first-name">First Name</label>
           <input id="first-name" type="text" formControlName="firstName">
